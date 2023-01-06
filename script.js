182f0c272d1b20fd9f52bc9a1d6a05006173daad
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2023-01-06 13:46:49 +0400

    added functions

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-console.log('Testing');
-console.log('Some feature');
-console.log('Another feature!');
+// console.log('Testing');
+// console.log('Some feature');
+// console.log('Another feature!');
+
+// 1. create a function that will take p tag content and rewrite it
+
+function takePar() {
+    // console.log(document.getElementsByTagName('P')[0].textContent);
+    document.getElementsByTagName('P')[0].textContent = "Changed Text";
+}
+
+takePar();
+// 2. create a function that will print the changed p tag content
+
+function showText() {
+    console.log(document.getElementsByTagName('p')[0].textContent);
+}
+
+showText();
